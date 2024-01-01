ad9d4d4a053b1393f463b69263b60805ac3e086f
ElectronicAPU <apurbanaskar3206@gmail.com>
2024-01-01 13:47:39 +0530

    search

---
+++ src/components/UserAvatar/UserListItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useAppContext } from "../../Context/AppContext";
+import { Avatar, Box, Text } from "@chakra-ui/react";
+
+const UserListItem = ({ handleFunction }) => {
+  const { user } = useAppContext();
+
+  return (
+    <>
+      <Box display="flex" py="10px" gap="10px">
+        <Avatar src={user.pic} name={user.name} />
+        <Box>
+          <Text fontWeight={700}>{user.name}</Text>
+          <Text fontSize={12} display="flex" gap="10px">
+            <Text style={{ fontWeight: 600 }}>Email:</Text> {user.email}
+          </Text>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default UserListItem;
--- src/components/miscellaneous/SideDrawer.js
+++ src/components/miscellaneous/SideDrawer.js
@@ -3,6 +3,14 @@ import {
   Avatar,
   Box,
   Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  Input,
   Menu,
   MenuButton,
   MenuDivider,
@@ -10,19 +18,28 @@ import {
   MenuList,
   Text,
   Tooltip,
+  useDisclosure,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { useAppContext } from "../../Context/AppContext";
 import ProfileModal from "./ProfileModal";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { DB } from "../../constant";
+import ChatLoading from "../ChatLoading";
+import UserListItem from "../UserAvatar/UserListItem";
+// import { useDisclosure } from "@chakra-ui/hooks";
 
 const SideDrawer = () => {
   const [search, setSearch] = useState("");
   const [searchResult, setSearchResult] = useState([]);
   const [loading, setLoading] = useState(false);
   const [lodingChat, setLoadingChat] = useState();
-  const { user } = useAppContext();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { user, token } = useAppContext();
 
+  const btnRef = React.useRef();
   const navigate = useNavigate();
 
   const logoutHandler = () => {
@@ -32,6 +49,38 @@ const SideDrawer = () => {
     navigate("/");
   };
 
+  const handleSearch = async () => {
+    if (!search) {
+      toast.warn("Input not be empty");
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const { data } = await axios.get(`${DB}/user?search=${search}`, config);
+
+      // if (data) {
+      //   console.log(data);
+      // }
+
+      setLoading(false);
+      setSearchResult(data);
+    } catch (error) {
+      setLoading(false);
+      toast.error("Faild to load the search result");
+      console.log(error);
+    }
+  };
+
+
+  const accessChat = (userId) => {
+    
+  }
   return (
     <>
       <Box
@@ -49,7 +98,7 @@ const SideDrawer = () => {
           hasArrow
           placement="bottom"
         >
-          <Button variant="ghost">
+          <Button ref={btnRef} variant="ghost" onClick={onOpen}>
             <i className="fa-solid fa-magnifying-glass"></i>
             <Text display={{ base: "none", md: "flex" }} px="4">
               Search User
@@ -84,6 +133,48 @@ const SideDrawer = () => {
           </Menu>
         </div>
       </Box>
+
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Create your account</DrawerHeader>
+
+          <DrawerBody>
+            <Box display="flex" pb={2}>
+              <Input
+                placeholder="Search by name or email"
+                mr={2}
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <Button onClick={handleSearch}>Go</Button>
+            </Box>
+            {loading ? (
+              <ChatLoading />
+            ) : (
+              searchResult.map((user) => (
+                <UserListItem
+                  key={user._id}
+                  handleFunction={() => accessChat(user._id)}
+                />
+              ))
+            )}
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant="outline" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="blue">Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
     </>
   );
 };
